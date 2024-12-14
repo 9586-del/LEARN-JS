3ab02fa59ef667afb5529044452877358f97fcf0
9586-del <radadiyabipin5@gmail.com>
2024-12-14 16:03:27 +0530

    ojo

--- JS/array.js
+++ JS/array.js
@@ -53,17 +53,46 @@
 // console.log(arr1.length);
 
 
-let arr1=[10,8,92,88.3,17,'amit',33];
-let arr2= new Array(5,8,9,3);
+// let arr1=[10,8,92,88.3,17,'amit',33];
+// let arr2= new Array(5,8,9,3);
 
 // // let ans=Array.isArray(arr1);             //check array or not                 //return boolean
-// let ans=arr1.concat(arr2);                   //combine two or more array         //return array
+// let ans=arr1.concat(arr2);                  //combine two or more array          //return array
 
 
 // //check
-// let ans = arr1.some((v) => v > 500);         //return boolean
-// let ans = arr1.find((v) => v > 500);              //return first match value,if not match return undefined
-// let ans = arr1.includes(18);                     //return boolean
+// let ans = arr1.some((v) => v > 500);        //return boolean
+// let ans = arr1.find((v) => v > 500);        //return first match value,if not match return undefined
+// let ans = arr1.includes(18);                //return boolean
 
-let ans = arr1.toString();                       //convert array to string            //return string
+// let ans = arr1.toString()                   //convert array to string            //return string
+// console.log(ans);
+
+let arr1=[10,8,92,88.3,92,33];
+// let arr2=['bipin','meet','jainik']
+
+// let ans= arr1.slice(2,5);                            //part of the array                                 //return array
+
+// let ans= arr1.filter((v,i) => v>50);                 //filtering array                                   //return array
+
+// let ans=arr1.reduce((acc,v,i) => acc*v, 1);          //return value
+
+let ans = arr1.indexOf(92);                          //first occurence of element's index                //return index
+
+// let ans = arr1.lastIndexOf(92);                      //last occurence of element's index                 //return index
+
+// let ans= arr1.every((v) => v>5);                     //return true if all the element fulfilled the condition      //return boolean
+
+// let ans = arr1.findIndex((v)  => v>90);              // return first occurence of element's index
+
+// let ans= arr1.fill(0,3,5)                            //return array
+
+// let ans=arr2.sort();                                 //charachters                                      //return array
+
+// let ans= arr1.sort((a,b) => a-b);                    //numbers                                          //return array
+
+// let ans= arr1.reverse();                             //reverse array                                    //return array
+
 console.log(ans);
+
+
---
+++ JS/arrayTask.js
@@ -0,0 +1,123 @@
+// arr1=[10,20,30,40,50,60];
+
+// Finding the maximum element in an array.
+
+// let ans=Math.max(...arr1);
+// console.log(ans);
+
+// let ans= arr1.sort((a,b) => b-a);  
+// console.log(ans[0]);
+
+
+// Finding the minimum element in an array.
+
+// let ans=Math.min(...arr1);
+// console.log(ans);
+
+// let ans= arr1.sort((a,b) => a-b); 
+// console.log(ans[0]);
+
+// Sorting an array in ascending order.
+// let ans= arr1.sort((a,b) => a-b);
+// console.log(ans);
+
+
+// Sorting an array in descending order.
+// let ans= arr1.sort((a,b) => b-a);
+// console.log(ans);
+
+
+// Reversing an array
+// let ans=arr1.reverse();
+// console.log(ans);
+
+// // Finding the sum of all elements in an array.
+// let ans=arr1.reduce((acc,v,i) => acc+v, 0);
+// console.log(ans);
+
+// // Finding the average of all elements in an array.
+// let ans=arr1.reduce((acc,v,i) => acc+v, 0);
+// console.log(ans/arr1.length);
+
+//  let arr1=[10,20,30,40,50,50,60];
+//   Checking if an array contains a specific element.
+// let ans=arr1.includes(50);
+// console.log(ans);
+
+// Removing duplicates from an array.
+    // let emptyArray=[];
+
+    // for(let i=0; i<arr1.length;i++){
+    //     if(!emptyArray.includes(arr1[i])){
+    //         emptyArray.push(arr1[i])
+    //     }
+    // }
+    // console.log(emptyArray);
+
+
+// Merging two arrays into a new array.
+// let arr2=[1,2,3,4];
+// let ans=arr1.concat(arr2);
+// console.log(ans);
+
+
+
+// Splitting an array into two arrays based on a condition.
+// let arr=[1,2,3,4,5,6,7,8,9,10];
+// let = arr.reduce((even,odd,num) => {
+//     if(num%2==0){
+//         even.push(num);
+//     } else {
+//         odd.push(num);
+//     }
+// },
+
+
+
+
+// let arr1=[10,20,30,40,50,60,70]
+// Finding the second largest element in an array.
+// let ans= arr1.sort((a,b) => b-a);
+// console.log(arr1[1]);
+
+
+
+// let arr1 = [1, 2, 3, 4];
+// let arr2 = [1, 2, 3, 4];
+// let ans = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+// console.log(ans);
+
+
+// // Finding the index of the first occurrence of an element in an array.
+// let arr1 = [10, 20, 30, 40, 50, 30];
+// let ans = arr1.indexOf(30);
+// console.log(ans);  
+
+
+// // Finding the index of the last occurrence of an element in an array.
+// let arr1 = [10, 20, 30, 40, 50, 30];
+// let ans = arr1.lastIndexOf(30);
+// console.log(ans);  
+
+
+//   Finding the first three maximum number's sum an array.
+// let arr1 = [10, 20, 4, 30, 50, 40];
+// arr1.sort((a, b) => b - a);
+// let sum = arr1[0] + arr1[1] + arr1[2];
+// console.log(sum);
+
+
+let arr1=[10,20,30,40,50,60];
+ans = arr1.sort((a,b) => a-b);
+
+
+let n = arr1.length;
+if (n % 2 === 1) {
+    return arr1[Math.floor(n / 2)];
+} else {
+    let mid1 = arr1[n / 2 - 1];
+    let mid2 = arr1[n / 2];
+    return (mid1 + mid2) / 2;
+}
+console.log(ans);
+
